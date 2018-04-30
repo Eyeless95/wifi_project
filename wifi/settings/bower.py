1b99f75b5f09635236f2df9f45e299a4914acf17
Bogdan Zagrebin <zagrebin.bogdan@gmail.com>
2018-04-30 22:41:05 +0300

    update requirements

--- wifi/settings/bower.py
+++ wifi/settings/bower.py
@@ -2,12 +2,13 @@ from wifi.settings.common import STATIC_DIR
 
 BOWER_COMPONENTS_ROOT = STATIC_DIR
 
-BOWER_INSTALLED_APPS = [
-    'bootstrap#3.3.7',
-    'jquery#3.2.1',
-    'datatables.net#1.10.16',
-    'datatables.net-bs4#3.2.2',
-    'datatables.net-fixedheader#3.1.3',
-    'datatables.net-responsive#2.2.0',
-    'datatables.net-responsive-bs#2.2.0',
-]
+BOWER_INSTALLED_APPS = ['bootstrap#3.3.7',
+                        'datatables.net#1.10.16',
+                        'datatables.net-bs#2.1.1',
+                        'datatables.net-bs4#3.2.2',
+                        'datatables.net-fixedheader#3.1.3',
+                        'datatables.net-responsive#2.2.0',
+                        'datatables.net-responsive-bs#2.2.0',
+                        'font-awesome#5.0.9',
+                        'jquery#3.2.1']
+
